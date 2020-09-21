67afa44f774d091e902bb8b0eabdfb3456244196
ranjangrg <ranjan_grg@hotmail.co.uk>
2020-09-21 20:51:00 +0000

    Read pixels now

--- src/main.cpp
+++ src/main.cpp
@@ -19,11 +19,39 @@ int main() {
 			}
 		}
 		std::cout << std::endl;
-		/* */
+		/* RED */
+		unsigned bytePerPixel = channels;
+		for (int pixelY = 0; pixelY < width; ++pixelY) {
+			for (int pixelX = 0; pixelX < height; ++pixelX) {
+				unsigned char* pixelOffset = imgData + (pixelX + height*pixelY) * bytePerPixel;
+				std::cout << std::setw(3) << int(pixelOffset[0]) << ' ';
+			}
+			std::cout << std::endl;
+		}
+		std::cout << std::endl;
+		/* GREEN */
+		for (int pixelY = 0; pixelY < width; ++pixelY) {
+			for (int pixelX = 0; pixelX < height; ++pixelX) {
+				unsigned char* pixelOffset = imgData + (pixelX + height*pixelY) * bytePerPixel;
+				std::cout << std::setw(3) << int(pixelOffset[1]) << ' ';
+			}
+			std::cout << std::endl;
+		}
+		std::cout << std::endl;
+		/* BLUE */
+		for (int pixelY = 0; pixelY < width; ++pixelY) {
+			for (int pixelX = 0; pixelX < height; ++pixelX) {
+				unsigned char* pixelOffset = imgData + (pixelX + height*pixelY) * bytePerPixel;
+				std::cout << std::setw(3) << int(pixelOffset[2]) << ' ';
+				pixelOffset[2] = (pixelOffset[2] - 2) % 255;
+			}
+			std::cout << std::endl;
+		}
+		std::cout << std::endl;
 	}
 
 	// Writing to image file 
-	//stbi_write_png("./data/tmp2.png", width, height, channels, imgData, width * channels);
+	stbi_write_png("./data/tmp2.png", width, height, channels, imgData, width * channels);
 	
 	stbi_image_free(imgData);
 
